7945ca4f7c34baac875c1a0c39328ecaf6c53294
tmheducation <68049755+tmheducation@users.noreply.github.com>
2022-10-12 11:58:29 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,80 +1,135 @@
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    # . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    # . . . #
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    # . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
-basic.showLeds(`
-    # . . . .
-    . . . . .
-    . # . . .
-    . . # . .
-    . . . . .
-    `)
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.setLedColor(0xff0000)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . # .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . # .
+        # . # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . # .
+        # . # # #
+        # . # # #
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . # .
+        # . # # #
+        # . # # #
+        # . # # #
+        `)
+    basic.showLeds(`
+        # . . # .
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        `)
+    basic.showLeds(`
+        # . . # .
+        # . # # #
+        # # # # #
+        # # # # #
+        # . # # #
+        `)
+    music.playMelody("- C - C - C - C ", 120)
+    basic.turnRgbLedOff()
+    basic.setLedColor(0x00ff00)
+    basic.showLeds(`
+        # . . # .
+        # . # # #
+        # # # # #
+        # # # # #
+        # . # # #
+        `)
+    basic.showLeds(`
+        # . . # .
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        `)
+    basic.showLeds(`
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        `)
+    basic.pause(200)
+    basic.showLeds(`
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        `)
+    basic.showLeds(`
+        # . # # #
+        # . # # #
+        # . # # #
+        # . # # #
+        # . . # .
+        `)
+    basic.showLeds(`
+        # . # # #
+        # . # # #
+        # . # # #
+        # . . # .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # . # # #
+        # . # # #
+        # . . # .
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # . # # #
+        # . . # .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # . . # .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.turnRgbLedOff()
+    basic.clearScreen()
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+	
+})
 basic.forever(function () {
 	
 })
